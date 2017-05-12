6f9c6e4d35bdefd121ca7962d1ff8d9fa17aad0f
Matthew S. MacLennan <msmaclennan@gmail.com>
2017-05-12 14:10:51 -0700

    Update Pathways2Impact.R

--- Pathways2Impact.R
+++ Pathways2Impact.R
@@ -5,6 +5,20 @@ i,"&format=XML")),useInternalNode=T)
 }
 subjectareas<-unique(unlist(lapply(listy,function(x) xpathApply(x,"//subjectarea",xmlValue))))
 #equals 140 subjects total
-uoa<-htmlTreeParse(getURL("http://impact.ref.ac.uk/casestudiesapi/REFAPI.svc/ListUnitsOfAssessment?format=XML"),useInternalNode=T)
+REF<-htmlTreeParse(getURL("http://impact.ref.ac.uk/casestudiesapi/REFAPI.svc/ListUnitsOfAssessment?format=XML"),useInternalNode=T)
 impacttype<-unique(unlist(lapply(listy,function(x) xpathApply(x,"//impacttype",xmlValue))))
-#What are the 60 impact topics? I cannot find them.
+#
+
+#########################################################################################################                                 
+REF<-htmlTreeParse("C:\\Users\\newguy\\Documents\\2014 REF\\CaseStudies.html",useInternalNode=T)
+#isolate impact reports
+REFimpact<-xpathApply(REF,"//div[@class='col-lg-12 impact readmore']",xmlValue)
+#Tidy up
+#REFimpactTidy<-lapply(REFimpact,function(x) strsplit(x,"\r\n"))
+REFimpactTidy<-lapply(REFimpact,function(x) strsplit(x,"(\r)+\\s*|(\n)+\\s*|(\n)$|(\r)$"))
+REFimpactTidy<-lapply(REFimpactTidy,function(x) strsplit(x[[1]],"(\r)+\\s*"))
+REFimpactTidy<-lapply(REFimpactTidy,function(x) strsplit(x[[1]],"(\n)+\\s*"))
+REFimpactSentence<-lapply(REFimpactTidy,function(x)paste0(x[[1]][-1],sep="",collapse=" "))
+REFimpactSent<-lapply(REFimpactSentence,function(x) strsplit(x[[1]],"(?<=\\. )",perl=T))  
+#
+########################################################################################################
