1f5a23a91af2cade2fd64de63912be869f3740c7
Matthew S. MacLennan <msmaclennan@gmail.com>
2017-05-15 14:52:20 -0700

    Update kubc.R

--- kubc.R
+++ kubc.R
@@ -1,1 +1,3 @@
+#knot, kchal, uchal represent statements concerning challenges to UBC, KMb and notions of KMb.
+ubc<-rbind(knot[,1:2],kchal[,1:2],uchal[,1:2])
 ubc[,2]<-gsub("^[0-9]{,2}\\.\\s+|[a-z]\\.\\s{2,}","",ubc[,2])
