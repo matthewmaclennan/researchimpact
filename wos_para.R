208b69a1f880a02b5613fea3dec0b7ebb3d39d1b
Matthew S. MacLennan <msmaclennan@gmail.com>
2018-01-21 08:37:14 -0800

    Update wos_para.R

--- wos_para.R
+++ wos_para.R
@@ -3,3 +3,4 @@ library(XML);library(RCurl);library(httr);
 a<-getURL("http://apps.webofknowledge.com/InboundService.do?mode=FullRecord&customersID=Alerting&IsProductCode=Yes&product=WOS&Init=Yes&Func=Frame&DestFail=http%3A%2F%2Fwww.webofknowledge.com&action=retrieve&SrcApp=Alerting&SrcAuth=Alerting&SID=6EwolUr17Pm2nWjaTtV&UT=WOS%3A000419574900008")
 a.p<-htmlTreeParse(a,useInternalNode=T)
 apv<-unlist(lapply(xpathApply(a.p,"//p[@class]"),xmlValue))
+abstract<-apv[grep("^[A-Za-z]",apv)][3]
